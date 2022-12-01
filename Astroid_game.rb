aaec52358c299f8df9454e0f4fb36c3245e40cfa
Yilikal250 <teshomeyilikal250@gmail.com>
2022-12-01 22:39:33 +0300

    gun man movement & image added

--- Astroid_game.rb
+++ Astroid_game.rb
@@ -1,22 +1,84 @@
 require 'ruby2d'
-gun_man = Image.new(
-  'shipsnew3new.png',
-  x: 400, y: 0,
-  width: 80, height: 80,
-  rotate: 90,
-  z: 10
+set width: 1040
+set height: 700
+set background:'blue'
+Image.new(
+  'house1.jpg',
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  x: 280,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  x: 560,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  x: 860,
+  width: 180, height: 180
 )
 
+Image.new(
+  'house1.jpg',
+  y: 260,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  y: 260, x: 280,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  y: 260, x: 560,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  y: 260, x: 860,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  y: 520,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  y: 520, x: 280,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  y: 520, x: 560,
+  width: 180, height: 180
+)
+Image.new(
+  'house1.jpg',
+  y: 520, x: 860,
+  width: 180, height: 180
+)
+
+sprite=Sprite.new(
+'gunMan1.png',
+x: 100,
+y:380,
+clip_width: 60
+)
 on :key_held do |event|
-  case event.key
-  when 'up'
-    gun_man.y -= 2
-  when 'down'
-    gun_man.y += 2
-  when 'left'
-    gun_man.x -= 2
-  when 'right'
-    gun_man.x += 2
-  end
-end
+case event.key
+when 'up'
+  sprite.y-=2
+when 'down'
+  sprite.y+=2
+when 'left'
+  sprite.x-=2
+when 'right'
+  sprite.x+=2      
+end 
+end  
 show
